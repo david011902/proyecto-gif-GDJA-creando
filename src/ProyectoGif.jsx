77b1d2f7336e4e12b9bb79be882051c442922043
david011902 <davidlira0119@outlook.es>
2024-11-29 23:03:11 -0600

    ProyectoGif_Inicio

---
+++ src/ProyectoGif.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState} from 'react';
+export const ProyectoGif = () => {
+    const [categories, setCategories] = useState(['One Punch'])
+return (
+    <>
+    <div>
+      {/*titulo*/}
+      <h1>ProyectoGif</h1>
+      {/*Input*/}
+      
+      {/*Listado de Gifs */}
+      {/*Gif Item */}
+    </div>
+    
+    </>
+)
+}
+
+/*
+export default ProyectoGif
+function (a){
+    return a + 100;
+}
+
+(a) => a + 100;
+*/
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+/*import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'*/
+// Importamos la biblioteca de React
+import React from 'react'
+// Importamos la biblioteca ReactDOM, que proporciona métodos específicos del DOM para 
+React
+import ReactDOM from 'react-dom/client'
+// Importamos un componente llamado ProyectoGif desde otro archivo en el mismo directorio
+import { ProyectoGif } from './ProyectoGif'
+// Creamos un nuevo contenedor de raíz React en el elemento con el id 'root'
+// y renderizamos el componente ProyectoGif en él
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {}
+    <ProyectoGif/>
+  </React.StrictMode>,
+)
